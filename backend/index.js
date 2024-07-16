38c3eca3b0d12eb4dc7aae1618a3b221af76fc50
Gr701 <72044825+Gr701@users.noreply.github.com>
2024-07-16 07:35:14 +0300

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -17,24 +17,6 @@ const requestLogger = (request, response, next) => {
 }
 app.use(requestLogger)
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2", 
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3", 
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  },
-]
-
 app.get('/', (request, response) => {
   response.send('<h1>Hellow World!</h1>')
 })
@@ -57,15 +39,7 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  Note.findByIdAndDelete(request.params.id)
-    .then(note => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
-})
-
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (!body.content) {
@@ -79,21 +53,31 @@ app.post('/api/notes', (request, response) => {
     important: Boolean(body.important) || false,
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
-  const note = {
-    content: body.content,
-    important: body.important
-  }
+  const {content, important} = request.body
 
-  Note.findByIdAndUpdate(request.params.id, note, {new: true})
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    {content, important}, 
+    {new: true, runValidators: true, context: 'query'}
+  )
     .then(updatedNote => {
       response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(note => {
+      response.status(204).end()
     })
     .catch(error => next(error))
 })
@@ -107,6 +91,8 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message) 
   if (error.name === 'CastError') {
     return response.status(400).send({error: 'malformatted id'})
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
   }
   next(error)
 }
